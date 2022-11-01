31bdd5f23479634037380d15bb65278dc28bf381
marcosestebandev <marcosestebangodoy@hotmail.com>
2022-11-01 12:09:37 -0300

    first commit

---
+++ src/api/products/model.ts
@@ -0,0 +1,64 @@
+const Contenedor = require('../../store/contenedor')
+const productsDB = new Contenedor('products')
+
+function addProduct({
+  title,
+  price,
+  thumbnail,
+  description,
+  code,
+  stock,
+  timestamp,
+}) {
+  return productsDB.save({
+    title,
+    price,
+    thumbnail,
+    description,
+    code,
+    stock,
+    timestamp,
+  })
+}
+
+function getAllProducts() {
+  return productsDB.getAll()
+}
+
+function getProductById(id) {
+  return productsDB.getById(id)
+}
+
+function updateProductById({
+  id,
+  title,
+  price,
+  thumbnail,
+  description,
+  code,
+  stock,
+  timestamp,
+}) {
+  return productsDB.updateById({
+    id,
+    title,
+    price,
+    thumbnail,
+    description,
+    code,
+    stock,
+    timestamp,
+  })
+}
+
+function deleteProductById(id) {
+  return productsDB.deleteById(id)
+}
+
+export default {
+  addProduct,
+  getAllProducts,
+  getProductById,
+  updateProductById,
+  deleteProductById,
+}
